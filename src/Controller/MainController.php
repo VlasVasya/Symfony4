e9c9e74014a4b77c98ea0a3c6e670387a6c9aace
Vasua <vlasukvasua@gmail.com>
2019-05-11 15:14:29 +0300

    symfony4

---
+++ src/Controller/MainController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Notes;
+use App\Form\NotesType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request)
+    {
+        $em=$this->getDoctrine()->getManager();
+
+        $note= new Notes();
+        $form=$this->createForm(NotesType::class, $note);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted($request) && $form->isValid()){
+            $data=$form->getData();
+
+            $em->persist($note);
+            $em->flush();
+
+            return $this->redirectToRoute("index");
+        }
+
+        $notes= $em->getRepository(Notes::class)->findAll();
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+            'form'=> $form->createView(),
+            'notes'=>$notes
+        ]);
+    }
+    /**
+     * @Route("/remove/{note}", name="remove_note")
+     */
+    public function removeNote(Notes $note, Request $request){
+       $em= $this->getDoctrine()->getManager();
+       $em->remove($note);
+       $em->flush();
+
+       return $this->redirectToRoute("index");
+    }
+}
